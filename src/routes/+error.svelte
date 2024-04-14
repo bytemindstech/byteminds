9da2f2472389c105868306cb07a2256b5cfe3d24
Jhenbert <jvv202726@gmail.com>
2024-04-15 07:44:30 +0800

    error page improvement

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,4 +2,11 @@
   import { page } from "$app/stores";
 </script>
 
-<h1 class="h1 text-center mt-5">{$page.status}: {$page.error?.message}</h1>
+<section>
+  <div class="container">
+    <div class="flex items-center justify-center min-h-screen">
+      <h1 class="h1 md:text-7xl">{$page.status} |</h1>
+      <span><p class="md:text-2xl">{$page.error?.message}</p></span>
+    </div>
+  </div>
+</section>
